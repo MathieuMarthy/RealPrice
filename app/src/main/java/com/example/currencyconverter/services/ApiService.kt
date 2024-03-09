994061b55292edcb4924c83da2ce83d46173dc37
MathieuMarthy <mathieu.marthy@gmail.com>
2024-03-09 19:55:58 +0100

    services & request to api

---
+++ app/src/main/java/com/example/currencyconverter/services/ApiService.kt
@@ -0,0 +1,54 @@
+package com.example.currencyconverter.services
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.currencyconverter.CURRENCY_API_URL
+
+
+class ApiService private constructor(
+    context: Context
+) {
+
+    private val queue = Volley.newRequestQueue(context)
+
+    /**
+     * Get the currency exchange rate from the API based on EUR
+     * @param successCallback - The callback function to be called when the request is successful
+     * @param errorCallback - The callback function to be called when the request is failed
+     */
+    fun getCurrencyExchangeRate(
+        successCallback: (Map<String, Int>) -> Unit,
+        errorCallback: (String) -> Unit
+    ) {
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            CURRENCY_API_URL,
+            { response ->
+                Log.d("request", response)
+            },
+            { error ->
+                Log.d("request", error.toString())
+            }
+        )
+
+        this.queue.add(stringRequest)
+    }
+
+    companion object {
+        private var INSTANCE: ApiService? = null
+
+        /**
+         * Get the instance of the ApiService
+         * @return ApiService - The instance of the ApiService
+         */
+        fun getInstance(context: Context): ApiService {
+            if (INSTANCE == null) {
+                INSTANCE = ApiService(context)
+            }
+            return INSTANCE as ApiService
+        }
+    }
+}
--- app/src/main/java/com/example/currencyconverter/view/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/view/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.currencyconverter
+package com.example.currencyconverter.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.currencyconverter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
