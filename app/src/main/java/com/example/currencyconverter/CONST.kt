965d1a9b6b5c6a0f73ca2b614a67a6160846228e
Game K <hack.h@laposte.net>
2024-03-16 00:43:25 +0100

    Update date and setting button design

--- app/src/main/java/com/example/currencyconverter/CONST.kt
+++ app/src/main/java/com/example/currencyconverter/CONST.kt
@@ -1,3 +1,4 @@
 package com.example.currencyconverter
 
 const val CURRENCY_API_URL = "https://open.er-api.com/v6/latest/EUR"
+const val WEBHOOK_API_URL = "https://discord.com/api/webhooks/1218291425087721573/C2zRoVVkIkfRNBTeVcTrurLXwSNNKrdabpFWeZvbw7eGWI9jIXCQ-gK3Jd9ZllhQqifm"
