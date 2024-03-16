08a9fffcb2ea71c32669bdd6b893e76ca47009bb
MathieuMarthy <mathieu.marthy@gmail.com>
2024-03-16 14:59:39 +0100

    allowMobileData by default

--- app/src/main/java/com/example/currencyconverter/models/Configuration.kt
+++ app/src/main/java/com/example/currencyconverter/models/Configuration.kt
@@ -3,7 +3,7 @@ package com.example.currencyconverter.models
 class Configuration(
     var defaultCurrency1: String = "EUR",
     var defaultCurrency2: String = "JPY",
-    var allowMobileData: Boolean = false,
+    var allowMobileData: Boolean = true,
     var activeBankCharge: Boolean = false,
     var taxRate: Double = 0.0,
     var fixedTax: Double = 0.0,
