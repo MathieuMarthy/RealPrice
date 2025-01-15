54067de228ee5d2bef7820caec9a5480a78c181e
MathieuMarthy <mathieu.marthy@gmail.com>
2025-01-15 22:36:16 +0100

    change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.app.realprice"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
