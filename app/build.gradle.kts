070c83f5273b9949b135d4b921709e0e0d1948eb
mathieuMarthy <mathieu.marthy@gmail.com>
2025-09-20 15:35:27 +0200

    update version code and name to 5 and 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.realprice"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
